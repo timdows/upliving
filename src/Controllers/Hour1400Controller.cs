f0f7bac7804bbd49de1e0fade258a248f3eae639
Timdows <tim@inexpro.nl>
2019-04-15 17:05:01 +0200

    Implement usecase

--- src/Controllers/Hour1400Controller.cs
+++ src/Controllers/Hour1400Controller.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Upliving.UseCases.Hour1400;
 
 namespace Upliving.Controllers
 {
-	public class Hour1400Controller
+	[Route("api/[controller]/[action]")]
+	public class Hour1400Controller : Controller
 	{
+		private readonly IMediator _mediator;
+
+		public Hour1400Controller(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpPost]
+		[ProducesResponseType(typeof(Hour1400UploadResponse), 200)]
+		[ProducesResponseType(500)]
+		public async Task<IActionResult> Hour1400Upload([FromBody] Hour1400UploadRequest request)
+		{
+			var result = await _mediator.Send(request);
+			return Ok(result);
+		}
 	}
 }
